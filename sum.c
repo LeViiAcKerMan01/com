fa9e1780fe3600923992e2e190a1a97699da28e3
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2023-07-09 00:21:55 +0530

    LeVii added new files into the Project

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int sum;
+	scanf("Enter first number : %d\n", &a);
+	scanf("Enter Second number : %d\n", &b);
+	sum = a+b;
+	printf("The sum of two integer values is : %d", sum);
+	return 0;
+}
+//Jisko hasi aari hai usko btadu coding k naam pe itna hi aata h mujhe bas!
