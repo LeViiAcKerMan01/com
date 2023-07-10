e28f93231900e277d8c0fd11eb2efeaefbf35ce0
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2023-07-10 22:58:56 +0530

    LeVii added new files to the Project

---
+++ input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int sum;
+	scanf("Enter first number: %d", &a);
+	scanf("Enter second number: %d", &b);
+	sum = a+b;
+	printf("The sum is : %d",sum);
+	return 0;
+
+}
