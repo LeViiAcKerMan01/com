4c2cfbaff8b3b427dfb39885b2f30400d5edb60f
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2023-07-12 10:59:36 +0530

    LeVii modified one file

--- string.c
+++ string.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-	char x[40] = "Priyanshu says";
+	char x[40] = "Priyanshu says ";
 	char y[] = "C is great";
-	strcat(a,b);
+	strcat(x,y);
 	printf("%s", x);
 	return 0;
 }
