a34cced093c7606406effe6c857e49cf868afd4b
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2023-07-13 00:14:22 +0530

    LeVii added new files into the Project

---
+++ dict.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	char ist[20]= "Hello, World!";
+    char s[100]="Life is beautiful";
+    scanf("%s\n", s);
+    printf("%s\n", ist);
+    printf("%s", s[100]);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
---
+++ sqr.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int area;
+	area = a*a;
+	printf("%d",area);
+	return 0;
+}
