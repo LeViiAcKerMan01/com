fc182b48056986051ba881f5148ab99ca5422cd6
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2023-07-14 09:01:55 +0530

    LeVii added new files

---
+++ file.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	const double PI= 3.1415926536;
+	printf("Memory constant is given as: %.1f", PI);
+	printf("Literal constant is given as: %.1lf", PI);
+	printf("Defined constant is given as: %.1f", PI);
+	return 0;
+}
