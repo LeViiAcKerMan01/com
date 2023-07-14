3823051b19ab414519b7d629d16172b4195c44d2
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2023-07-15 00:28:19 +0530

    LeVii modified new files into the Project

--- for.c
+++ for.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
 {
+    int n;
 	int i;
+	scanf("%d", &n);
 	for(i=1; i<=10; i++){
-		printf("%d\n" i);
-                i = i+1;
+    printf("%d*%d = %d\n", n, i, n*i);
 	}
-        return 0;
+	return 0;
 }
