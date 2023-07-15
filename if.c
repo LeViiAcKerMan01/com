491c5eae16c929e286f0d61a078f1fde749d0211
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2023-07-16 00:13:35 +0530

    LeVii modified some files into the Project

---
+++ if.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int age = 25;
+	int Vage = 18;
+	if(age>Vage){
+		printf("Old enough to vote!");
+	}
+	else{
+		printf("Not old enough to vote");
+	}
+	return 0;
+}
+
